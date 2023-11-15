2a151c2354126a63887e24092d9c806a67606a3b
lucas <francisco.lucas0507@gmail.com>
2023-11-14 21:24:49 -0300

    Initial Layout

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,17 @@
+const colors = require("tailwindcss/colors");
+
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        background: colors.stone["300"],
+        accent: colors.stone["500"],
+        highlight: colors.pink["600"],
+        "highlight-off": colors.pink["500"],
+      },
+    },
+  },
+  plugins: [],
+};
