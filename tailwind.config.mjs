611ce244cb5cfebd2a39431408ba0dde697c8319
lucas <francisco.lucas0507@gmail.com>
2023-11-18 08:36:59 -0300

    Backgrounds and Colors

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -6,10 +6,18 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: colors.stone["300"],
+        background: colors.stone["600"],
         accent: colors.stone["500"],
         highlight: colors.pink["600"],
+        "regular-text": colors.neutral["300"],
         "highlight-off": colors.pink["500"],
+        "footer-link": colors.stone["400"],
+      },
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        blobby_noise: "url('/blobby_noise.svg')",
+        dark_noise: "url('/dark_noise.svg')",
+        noise: "url('/noise.svg')",
       },
     },
   },
