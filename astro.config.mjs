639c91d74b8fd3767dadc0b37579aaf352cbee27
Lucas Costa <francisco.lucas0507@gmail.com>
2023-12-11 01:40:08 -0300

    mdx integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,33 +1,34 @@
 import { defineConfig } from "astro/config";
-
 import vercel from "@astrojs/vercel/static";
 import tailwind from "@astrojs/tailwind";
 import preact from "@astrojs/preact";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   output: "static",
   adapter: vercel(),
-  integrations: [tailwind(), preact()],
+  integrations: [tailwind(), preact(), mdx()],
   markdown: {
     shikiConfig: {
-      theme: "rose-pine-moon",
-    },
+      theme: "rose-pine-moon"
+    }
   },
   redirects: {
-    "/": "/pt-br/",
+    "/": "/pt-br/"
   },
   i18n: {
     defaultLocale: "pt-br",
     locales: ["en", "pt-br"],
     routing: {
       prefixDefaultLocale: true,
-      strategy: "pathname",
-    },
+      strategy: "pathname"
+    }
   },
   vite: {
     build: {
-      minify: "terser",
-    },
-  },
+      minify: "terser"
+    }
+  }
 });
