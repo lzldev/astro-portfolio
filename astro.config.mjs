48095aee78ed58deeb4f57f916c2877a6aa2de25
Lucas Costa <francisco.lucas0507@gmail.com>
2025-01-02 21:28:47 -0300

    feat: astro sessions with upstash

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,21 @@
+import { loadEnv } from "vite"
 import { defineConfig } from "astro/config"
 import vercel from "@astrojs/vercel/static"
 import tailwind from "@astrojs/tailwind"
 import preact from "@astrojs/preact"
 
 import mdx from "@astrojs/mdx"
+
+const { KV_REST_API_URL, KV_REST_API_TOKEN } = loadEnv(
+  process.env.NODE_ENV || "development",
+  process.cwd(),
+  "",
+)
+
+console.log("options", {
+  url: KV_REST_API_URL,
+  token: KV_REST_API_TOKEN,
+})
 
 // https://astro.build/config
 export default defineConfig({
@@ -35,6 +47,19 @@ export default defineConfig({
   vite: {
     build: {
       minify: "terser",
+    },
+  },
+  experimental: {
+    session: {
+      driver: "upstash",
+      options: {
+        url: KV_REST_API_URL,
+        token: KV_REST_API_TOKEN,
+      },
+      cookie: {
+        name: "lzldev-portfolio",
+        sameSite: "lax",
+      },
     },
   },
 })
